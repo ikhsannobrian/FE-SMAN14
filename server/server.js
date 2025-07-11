b662f1e4f737aaa51f0bdcad67b4beda732f3acc
MuhammadWA <muhammadwildanavianto@gmail.com>
2025-07-12 02:16:32 +0700

    Fix CORS for local and deployed frontend

--- server/server.js
+++ server/server.js
@@ -16,7 +16,14 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",             // untuk development
+    "https://fe-sman14.railway.app"      // frontend deploy di Railway
+  ],
+  credentials: true                     
+}));
 app.use(express.json());
 
 // API Routes
